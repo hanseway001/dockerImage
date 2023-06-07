e4d1bb8d49e2162946303be4af8f2387cb6aa83f
Hanseway001 <wayne.hansen9276@stu.mtec.edu>
2023-06-07 06:55:21 -0600

    trying to figure out why my docker is rebooting"

--- index.js
+++ index.js
@@ -2,16 +2,14 @@
 
 const express = require('express');
 
-// Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world\n');
+    res.send('Hello world\n This is a test to see if my Docker image works!');
 });
 
 app.listen(PORT, HOST);
-console.log(`Express server listening on http://${HOST}:${PORT}`);
+console.log(`listening on http://${HOST}:${PORT}`);
